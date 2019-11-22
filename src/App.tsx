5859f262e15a40ff99a5a57463c78aa400b1469e
Heidi Laursen <heidilaursen@yahoo.com>
2019-11-22 15:34:13 -0800

    bad background image

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import './App.css';
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
-
-
-
+// import tools from './img/tools.jpg'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <div className="background-img">
+
+      </div>
+        {/* <img
+          className="background-img"
+          src={tools}
+          alt="tools"
+        /> */}
       <Header />
       <Body />
       <Footer />
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export default function Footer() {
     return (        
     <footer>
-        <h4><a href="tel:3602980528" target="_blank">360.298.0528</a></h4> | <h4><a href="mailto:heidimlaursen@gmail.com" >heidimlaursen@gmail.com</a></h4>
+        <h4><a href="tel:3602980528" target="_blank" rel="noopener noreferrer" >360.298.0528</a></h4> | <h4><a href="mailto:heidimlaursen@gmail.com" target="_blank" rel="noopener noreferrer" >heidimlaursen@gmail.com</a></h4>
         <h4><a href="https://github.com/pnwlady" target="_blank" rel="noopener noreferrer" >Github</a> |  <a href="https://www.linkedin.com/in/heidilaursen/" target="_blank" rel="noopener noreferrer" >LinkedIn</a></h4>
         <p>Copyright ©2019.</p>
     </footer>
